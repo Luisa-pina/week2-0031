5225f32f1f3e0094527e201899b10fcdf33b3ecf
Luisa Pina <luisa_pina@Luisas-MacBook-Air.local>
2022-09-15 12:06:53 -0400

    Initial commit

---
+++ Input week2.py
@@ -0,0 +1,3 @@
+your_name = input("Tell me your name")
+your_name = input("tell me your major:")
+print("hello",your_name, "that is a great major", "Comp151 will help you")
---
+++ main.py
@@ -0,0 +1,6 @@
+message = "hello comp151 :("
+print(message)
+Message ="This is completely diffent"
+print(message)
+yourname = input(what is your name)
+print(hello, luisa)
